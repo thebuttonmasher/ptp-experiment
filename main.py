805dcaa2f737f3e7a56b35ec88295eddf1d10ec0
thebuttonmasher <tomer.hlprin22@gmail.com>
2025-06-07 22:19:16 +0300

    fix param

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ ptpip = PTPIPSession()
 ptpip.open(host='192.168.1.2', port=15740)
 while 1:
     cmd_to_send = int(input("input cmd"))
-    param = int(input("and param"))
+    param = input("and param")
+    if param:
+        param = int(param)
     ptip_cmd = PtpIpCmdRequest(cmd=cmd_to_send, param1=param)
     ptpip.send_recieve_ptpip_packet(ptip_cmd, ptpip.session)

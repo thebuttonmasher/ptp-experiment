2dc4e2e885e478a66bbf72d6745113bd6aed44e6
thebuttonmasher <tomer.hlprin22@gmail.com>
2025-06-07 21:50:20 +0300

    fix hostname encoding

--- packet_types/PTPInitCommand.py
+++ packet_types/PTPInitCommand.py
@@ -12,7 +12,7 @@ class PtpIpInitCmdReq(PtpIpPacket):
             guid = uuid.uuid4()
             self.guid = guid.bytes
             self.hostname = socket.gethostname() + '\x00'
-            self.hostname = self.hostname.encode()
+            self.hostname = self.hostname.encode("utf-16")
         else:
             self.guid = data[0:16]
             self.hostname = data[16:0]

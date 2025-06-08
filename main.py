57306dcf2ad35ce4a40470f5835e235f0b53f073
thebuttonmasher <tomer.hlprin22@gmail.com>
2025-06-08 22:09:34 +0300

    changed commands

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ ptip_cmd1 = PtpIpCmdRequest(cmd=0x9114, param1=0x1, tid=ptpip.transaction_id)
 ptip_cmd2 = PtpIpCmdRequest(cmd=0x9115, param1=0x1, tid=ptpip.transaction_id)
 ptip_cmd3 = PtpIpCmdRequest(cmd=0x9128, param1=0x1, param2=0x0, tid=ptpip.transaction_id)
 ptip_cmd4 = PtpIpCmdRequest(cmd=0x9128, param1=0x2, param2=0x0, tid=ptpip.transaction_id)
-#ptip_cmd5 = PtpIpCmdRequest(cmd=0x911a, param1=0xfffff806, param2=0x00000100, param3=0x0)
-#ptip_cmd6 = PtpIpCmdRequest(cmd=0x9110, data_phase=0x01, param1=0xc, param2=0xd1b8, param3=0x0, tid=ptpip.transaction_id)
+ptip_cmd5 = PtpIpCmdRequest(cmd=0x9128, param1=0x3, param2=0x0, tid=ptpip.transaction_id)
+ptip_cmd6 = PtpIpCmdRequest(cmd=0x9129, param1=0x3, param2=0x0, tid=ptpip.transaction_id)
 ptip_cmd7 = PtpIpCmdRequest(cmd=0x9129, param1=0x2, param2=0x0, tid=ptpip.transaction_id)# param1=0x00010001, tid=ptpip.transaction_id)
 ptip_cmd8 = PtpIpCmdRequest(cmd=0x9129, param1=0x1, param2=0x0,tid=ptpip.transaction_id)# param1=0x00020000, tid=ptpip.transaction_id)
-cmds = [ptip_cmd1, ptip_cmd2, ptip_cmd3, ptip_cmd4, ptip_cmd7, ptip_cmd8]
+cmds = [ptip_cmd1, ptip_cmd2, ptip_cmd3, ptip_cmd4, ptip_cmd5, ptip_cmd6, ptip_cmd7, ptip_cmd8]
 for cmd in cmds:
     input("press enter to send next command...")
     ptpip.send_recieve_ptpip_packet(cmd, ptpip.session)
